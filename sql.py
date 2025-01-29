60a990c80fdea29072d4cd025263d7427348473e
Taleb88 <tdamaree18@gmail.com>
2025-01-28 22:41:24 -0500

    developed sql database and connected successfully via psycopg2

--- sql.py
+++ sql.py
@@ -1,2 +1,15 @@
 import psycopg2
 
+conn = psycopg2.connect(database='denver_crime_rate',
+                        user='postgres', password='root',
+                        host='127.0.0.1', port='5432'
+
+)
+
+conn.autocommit = True
+cursor = conn.cursor()
+
+if conn:
+    print('db connection = success')
+
+sql = ''''''
